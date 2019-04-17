a6d17841b800c0a16c9f62dc8a4349394aad9439
ritvij-saxena <saxenaritvij@gmail.com>
2019-04-17 13:42:09 -0500

    Initial Commit

---
+++ app/src/main/java/com/rj/stock/MyViewHolder.java
@@ -0,0 +1,28 @@
+package com.rj.stock;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+class MyViewHolder extends RecyclerView.ViewHolder {
+    public TextView companyName;
+    public TextView companySymbol;
+    public TextView price;
+    public TextView priceChange;
+    public TextView changePercentage;
+    public ImageView arrow;
+    public View dividerView;
+
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+        companyName = itemView.findViewById(R.id.companynameTextView);
+        companySymbol = itemView.findViewById(R.id.companysymbolTextView);
+        price = itemView.findViewById(R.id.priceTextView);
+        priceChange = itemView.findViewById(R.id.pricechangeTextView);
+        changePercentage = itemView.findViewById(R.id.changepercentageTextView);
+        arrow = itemView.findViewById(R.id.arrowImageView);
+        dividerView = itemView.findViewById(R.id.divider);
+    }
+}
